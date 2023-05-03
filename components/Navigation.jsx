2d0595bff8bcd9e28bc2c6c8017fdc08b11f7331
Mark Donohue <markbdonohue@gmail.com>
2023-05-03 18:46:05 -0400

    Update nav bar to include logo

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 
 const Navigation = () => {
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100">
-      <div className="flex justify-between items-center w-full h-full px-2"></div>
-      <Image src="../public/assets/MD.png" alt="/" />
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2">
+      <Image src="/../public/assets/MD.png" alt="/" width="70" height="40" />
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Navigation />
+    </main>
+  );
 }
