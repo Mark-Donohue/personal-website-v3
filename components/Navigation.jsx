0bdfea53aa4cdcac0855bfabde04ffa38d79cc25
Mark Donohue <markbdonohue@gmail.com>
2023-05-08 17:34:53 -0400

    Add menu for non-mobile use

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full px-2">
-      <Image src="/../public/assets/MD.png" alt="/" width="70" height="40" />
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="/../public/assets/MD.png" alt="/" width="70" height="40" />
+      </div>
+      <div>
+        <ul className="hidden md:flex">
+          <li className="ml-10 text-sm uppercase hover:border-b">
+            <Link href="#">About</Link>
+          </li>
+          <li className="ml-10 text-sm uppercase hover:border-b">
+            <Link href="#">Resume</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
