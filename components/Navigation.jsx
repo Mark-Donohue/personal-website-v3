eef3c0d0f00dfbb20d78e5ce4bd12d583f415717
Mark Donohue <markbdonohue@gmail.com>
2023-05-10 18:11:20 -0400

    Fix divs in navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { HiMenu } from "react-icons/hi";
+import { IoMenu } from "react-icons/io5";
+import { MdMenu } from "react-icons/md"
 
 const Navigation = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image src="/../public/assets/MD.png" alt="/" width="70" height="40" />
-      </div>
-      <div>
-        <ul className="hidden md:flex">
-          <li className="ml-10 text-sm uppercase hover:border-b">
-            <Link href="#">About</Link>
-          </li>
-          <li className="ml-10 text-sm uppercase hover:border-b">
-            <Link href="#">Experience</Link>
-          </li>
-          <li className="ml-10 text-sm uppercase hover:border-b">
-            <Link href="#">Skills</Link>
-          </li>
-          <li className="ml-10 text-sm uppercase hover:border-b">
-            <Link href="#">Resume</Link>
-          </li>
-        </ul>
-        <div className="md:hidden">
-          <HiMenu/>
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-18">
+        <Image src="/../public/assets/MDLogo.png" alt="/" width="100" height="70" />
+        <div>
+          <ul className="hidden md:flex">
+            <li className="ml-10 text-md uppercase">
+              <Link href="#">About</Link>
+            </li>
+            <li className="ml-10 text-md uppercase">
+              <Link href="#">Experience</Link>
+            </li>
+            <li className="ml-10 text-md uppercase">
+              <Link href="#">Skills</Link>
+            </li>
+            <li className="ml-10 text-md uppercase">
+              <Link href="#">Resume</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="md:hidden hover:b">
+          <IoMenu size={50}/>
         </div>
       </div>
     </div>
