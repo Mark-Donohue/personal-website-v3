a289403af49d6a4331af805b3565e42a1179d212
Mark Donohue <markbdonohue@gmail.com>
2023-05-12 11:55:50 -0400

    Update nav to include new logo, update favicon

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -9,7 +9,7 @@ const Navigation = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-18">
-        <Image src="/../public/assets/MDLogo.png" alt="/" width="100" height="70" />
+        <Image src="/../public/assets/MDLogo2.png" alt="/" width="65" height="70" />
         <div>
           <ul className="hidden md:flex">
             <li className="ml-10 text-md uppercase">
@@ -27,7 +27,7 @@ const Navigation = () => {
           </ul>
         </div>
         <div className="md:hidden hover:b">
-          <IoMenu size={50}/>
+          <IoMenu size={65} color="#167bff"/>
         </div>
       </div>
     </div>
