c2009f8557d301e7316aa19f512afe09b925426f
Mark Donohue <markbdonohue@gmail.com>
2023-05-12 11:56:42 -0400

    Fix formatting

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,13 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { HiMenu } from "react-icons/hi";
 import { IoMenu } from "react-icons/io5";
-import { MdMenu } from "react-icons/md"
+import { MdMenu } from "react-icons/md";
 
 const Navigation = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-18">
-        <Image src="/../public/assets/MDLogo2.png" alt="/" width="65" height="70" />
+        <Image
+          src="/../public/assets/MDLogo2.png"
+          alt="/"
+          width="65"
+          height="70"
+        />
         <div>
           <ul className="hidden md:flex">
             <li className="ml-10 text-md uppercase">
@@ -27,7 +32,7 @@ const Navigation = () => {
           </ul>
         </div>
         <div className="md:hidden hover:b">
-          <IoMenu size={65} color="#167bff"/>
+          <IoMenu size={65} color="#167bff" />
         </div>
       </div>
     </div>
