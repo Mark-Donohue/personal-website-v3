6aa71e86626488f3f084802eb8a0f8610e098839
Mark Donohue <markbdonohue@gmail.com>
2023-06-11 13:05:21 -0400

    Remove imports

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { IoMenu, IoClose } from "react-icons/io5";
-import { SiLinkedin, SiGithub, SiAngellist } from "react-icons/si";
 
 const Navigation = () => {
   const [sideMenu, setSideMenu] = useState(false);
