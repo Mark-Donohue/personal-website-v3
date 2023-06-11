e041c080c7e4ac2680dcd8f10927c87702077e71
Mark Donohue <markbdonohue@gmail.com>
2023-06-11 13:04:15 -0400

    Tweak hero placement

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+    <div className="w-full h-screen text-center pt-10">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 pt-2 flex justify-center items-center">
         <div>
-          <h1 className="py-4">
+          <h1 className="py-2">
             Hi, I&apos;m <span className="text-[#167BFF]">Mark</span>
           </h1>
-          <h1 className="py-2">I&apos;m a Software Engineer</h1>
+          <h1>I&apos;m a Software Engineer</h1>
           <p className="py-4 max-w-[70%] m-auto">
             I specialize in back-end API development but have experience with
             front-end technologies as well. Currently, I&apos;m focused on
