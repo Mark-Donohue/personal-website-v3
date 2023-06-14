af82984d222a0b0ecac95393f530578ac549eb7f
Mark Donohue <markbdonohue@gmail.com>
2023-06-14 08:14:04 -0400

    Remove unnecessary comments

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,6 @@ const About = () => {
   return (
     <div className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        {/* About Text */}
         <div className="col-span-2">
           <h2>About</h2>
           <p>I started programming my sophomore year of college. </p>
@@ -21,7 +20,6 @@ const About = () => {
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
-        {/* About Image */}
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-300 rounded-xl flex items-center justify-center p-2">
           <Image src={Headshot} className="rounded-xl" alt="/" />
         </div>
